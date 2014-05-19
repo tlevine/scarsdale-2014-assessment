c448c8313d79d03ae09ca1ce748d9270b259e7f3
Thomas Levine <_@thomaslevine.com>
2014-05-19 17:25:53 +0000

    -

--- stuff.r
+++ stuff.r
@@ -10,7 +10,14 @@ library(scarsdale)
 # scarsdale.RV_PROP_MAIN(MODEL_ISS) is whether there's an issue with the model estimate for the record.
 
 if (!('comparables' %in% ls())) {
-  comparables <- query('SELECT PROP_NBR, SALE_PRICE, MODEL_EST, SALE_MINUS_MODEL FROM RV_SALE_COMP;')
+  comparables <- query('
+SELECT RV_SALE_COMP.PROP_NBR, SALE_PRICE, RV_SALE_COMP.MODEL_EST, SALE_MINUS_MODEL
+FROM RV_SALE_COMP
+JOIN RV_PROP_MAIN
+WHERE RV_SALE_COMP.MODEL_EST IS NOT NULL
+  AND ATT_ISS = 0 AND MODEL_ISS = 0
+  AND RV_PROP_MAIN.PROP_NBR = RV_SALE_COMP.PROP_NBR
+;')
 }
 
 comparables.vars <- melt(comparables, id.vars = 'PROP_NBR')
@@ -33,7 +40,7 @@ p5 <- ggplot(comparables.vars) +
 
 
 # 1. What is the r^2 value of their model?
-
+answer.1 <- cor(comparables$MODEL_EST, comparables$SALE_PRICE)
 
 
 
@@ -45,3 +52,8 @@ p5 <- ggplot(comparables.vars) +
 
 
 # 3. How are the sale prices and model estimates distributed?
+# 3.1. Assuming normal distributions
+mean(comparables$MODEL_EST)
+mean(comparables$SALE_PRICE)
+sd(comparables$MODEL_EST)
+sd(comparables$SALE_PRICE)
