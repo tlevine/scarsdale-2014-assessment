7bfe2d748ba1aa9cb71a3f390564b24c0a5b3cb5
Thomas Levine <_@thomaslevine.com>
2014-05-19 16:38:01 +0000

    -

--- stuff.r
+++ stuff.r
@@ -1,2 +1,6 @@
 # install_github('scarsdale_assessment_database','tlevine')
 library(scarsdale)
+
+# Abridged data dictionary
+# scarsdale.RV_PROP_MAIN(ATT_ISS) is whether there's an issue with the data in the record.
+# scarsdale.RV_PROP_MAIN(MODEL_ISS) is whether there's an issue with the model estimate for the record.
