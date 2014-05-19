8cde0c3fd5087dcf76e7a2ce48ef6d71a12049ec
Thomas Levine <_@thomaslevine.com>
2014-05-19 23:48:37 +0000

    -

--- explaining-sale-price.r
+++ explaining-sale-price.r
@@ -31,6 +31,7 @@ comparables$model.error <- comparables$sale.price - comparables$model.estimate
 comparables$comparable.error <- comparables$sale.price - comparables$comparable.estimate
 p.errors <- ggplot(comparables) +
   ggtitle('Does the comparables process help us predict sale price?') +
+# aes(x = model.error, y = comparable.error, size = sale.price, color = comparable.estimate > model.estimate) +
   aes(x = model.error, y = comparable.error, size = sale.price) +
   coord_fixed() +
   scale_x_continuous('Model error: Difference between sale price and the modeled value', labels = dollar) + 
@@ -44,13 +45,26 @@ p.errors <- ggplot(comparables) +
 .annotation.label <- paste('The', c('model','comparable'), 'estimate is\ncloser to the sale price\nfor these properties.')
 p.error.differences <- ggplot(comparables) +
   aes(x = (abs(comparable.error) - abs(model.error))) +
+  aes(fill = comparable.estimate > model.estimate) +
   scale_x_continuous('Difference between absolute comparable error and absolute model error', labels = dollar) +
   geom_histogram(binwidth = 1e5) +
-  geom_vline(xintercept = 0, color = 'red', size = 2) +
+  geom_vline(xintercept = 0, color = 'black', size = 2) +
   ylab('Number of properties') +
   ggtitle('Does looking at comparables help us predict sale price?') +
-  annotate('text', label = .annotation.label, x = 3e5 * c(-1,1), y = 50, color = 'red', size = 7, hjust = c(1,0))
+  annotate('text', label = .annotation.label, x = 3e5 * c(-1,1), y = 50, color = 'black', size = 7, hjust = c(1,0))
+
+p.error.ratios <- ggplot(comparables) +
+  aes(x = model.error / sale.price, y = comparable.error / sale.price) +
+  geom_point() + coord_fixed() +
+  scale_x_continuous('Proportion of sale price that is the model error', labels = percent) +
+  scale_y_continuous('Proportion of sale price that is the comparable error', labels = percent)
+
+
+cor(comparables[c('model.estimate','comparable.estimate','sale.price')], method = 'spearman')
 
 # Test for a difference from zero on average/median
 test.1 <- t.test(abs(comparables$model.error) - abs(comparables$comparable.error))
 test.2 <- wilcox.test(abs(comparables$model.error) - abs(comparables$comparable.error))
+
+m.1 <- lm(sale.price ~ model.estimate, data = comparables)
+m.2 <- lm(sale.price ~ comparable.estimate, data = comparables)
