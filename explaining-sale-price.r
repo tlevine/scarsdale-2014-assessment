0d0ce3051d2df0d1a072be7b479e86802ffb3afb
Thomas Levine <_@thomaslevine.com>
2014-05-19 22:43:02 +0000

    write a query

---
+++ explaining-sale-price.r
@@ -0,0 +1,24 @@
+# install_github('scarsdale_assessment_database','tlevine')
+library(ggplot2)
+library(scales)
+library(reshape2)
+
+library(scarsdale)
+
+# From 6 pm Monday
+if (!('comparables' %in% ls())) {
+  comparables <- query('
+SELECT
+  RV_SALE_COMP.PROP_NBR "property.number",
+  RV_SALE_COMP.MODEL_EST "model.estimate.as.comparable",
+  RV_PROP_MAIN.MODEL_EST "model.estimate.as.subject",
+  RV_SALE_COMP.SALE_PRICE "sale.price",
+  RV_PROP_MAIN.TOTAL_PRE_2014 "comparable.estimate"
+FROM RV_SALE_COMP
+JOIN RV_PROP_MAIN
+WHERE RV_SALE_COMP.MODEL_EST IS NOT NULL
+  AND ATT_ISS = 0 AND MODEL_ISS = 0
+  AND RV_PROP_MAIN.PROP_NBR = RV_SALE_COMP.PROP_NBR
+;')
+}
+
