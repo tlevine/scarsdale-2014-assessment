642b18f1937a9778ad5b88c24f2438684739b07e
Thomas Levine <_@thomaslevine.com>
2014-05-20 13:56:06 +0000

    save plots

--- explaining-sale-price.r
+++ explaining-sale-price.r
@@ -31,27 +31,25 @@ comparables$model.error <- comparables$sale.price - comparables$model.estimate
 comparables$comparable.error <- comparables$sale.price - comparables$comparable.estimate
 p.errors <- ggplot(comparables) +
   ggtitle('Does the comparables process help us predict sale price?') +
-# aes(x = model.error, y = comparable.error, size = sale.price, color = comparable.estimate > model.estimate) +
   aes(x = model.error, y = comparable.error, size = sale.price) +
   coord_fixed() +
   scale_x_continuous('Model error: Difference between sale price and the modeled value', labels = dollar) + 
-  scale_y_continuous('Comparable error: Difference between the sale price and the comparable-adjusted assigned value', labels = dollar) +
+  scale_y_continuous('Comparable error: Difference between the sale price\nand the comparable-adjusted assigned value', labels = dollar) +
   scale_size_continuous('Sale price of house', labels = dollar) +
   geom_abline(slope = 1, color = 'red') + geom_abline(slope = -1, color = 'red') +
+  geom_point() +
   annotate('text', label = 'Model estimate\nis better.', y = c(-1e6, 1e6), x = 0, color = 'red') +
-  annotate('text', label = 'Comparable estimate\nis better.', x = c(-1e6, 1e6), y = 0, color = 'red') +
-  geom_point()
+  annotate('text', label = 'Comparable estimate\nis better.', x = c(-1e6, 1e6), y = 0, color = 'red')
 
 .annotation.label <- paste('The', c('model','comparable'), 'estimate is\ncloser to the sale price\nfor these properties.')
 p.error.differences <- ggplot(comparables) +
   aes(x = (abs(comparable.error) - abs(model.error))) +
-  aes(fill = comparable.estimate > model.estimate) +
   scale_x_continuous('Difference between absolute comparable error and absolute model error', labels = dollar) +
   geom_histogram(binwidth = 1e5) +
-  geom_vline(xintercept = 0, color = 'black', size = 2) +
+  geom_vline(xintercept = 0, color = 'red', size = 2) +
   ylab('Number of properties') +
   ggtitle('Does looking at comparables help us predict sale price?') +
-  annotate('text', label = .annotation.label, x = 3e5 * c(-1,1), y = 50, color = 'black', size = 7, hjust = c(1,0))
+  annotate('text', label = .annotation.label, x = 3e5 * c(-1,1), y = 50, color = 'red', size = 7, hjust = c(1,0))
 
 p.error.ratios <- ggplot(comparables) +
   aes(x = model.error / sale.price, y = comparable.error / sale.price) +
@@ -67,3 +65,7 @@ test.2 <- wilcox.test(abs(comparables$model.error) - abs(comparables$comparable.
 
 m.1 <- lm(sale.price ~ model.estimate, data = comparables)
 m.2 <- lm(sale.price ~ comparable.estimate, data = comparables)
+
+# Draw
+ggsave('errors.png', p.errors, width = 11, height = 6)
+ggsave('error-differences.png', p.error.differences, width = 11, height = 6)
